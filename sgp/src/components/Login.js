3e822acdf52336b08978a1e9e2253ea1e23299a9
Charly <carlosjoelsalda@gmail.com>
2021-09-24 02:04:47 -0300

    login terminado con background de ejemplo

--- sgp/src/components/Login.js
+++ sgp/src/components/Login.js
@@ -1,7 +1,8 @@
-import React, {useRef, useState} from 'react';
+import React, { useRef, useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import '../styles/Login.css'
 import { useHistory } from 'react-router';
+import background from '../assets/background.jpg'
 
 
 const Login = () => {
@@ -18,17 +19,17 @@ const Login = () => {
     const handleInputChange = (event) => {
         setData({
             ...data,
-            [event.target.name] : event.target.value
+            [event.target.name]: event.target.value
         })
         inputRef.current.disabled = !checkEmail() || !checkPassword()
     };
-    
 
-    const checkEmail =  () => {
-        const emailInput =  (emailRef.current.value).trim()
+
+    const checkEmail = () => {
+        const emailInput = (emailRef.current.value).trim()
         const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-        return emailInput.length >= 5 && re.test(String(emailInput).toLowerCase())  
+        return emailInput.length >= 5 && re.test(String(emailInput).toLowerCase())
     };
 
     const checkPassword = () => {
@@ -47,24 +48,29 @@ const Login = () => {
     }
 
     return (
-        <div className="divFormLogin">
-            <Form onSubmit={goToHome}>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Control ref={emailRef} className="boxInput" name="email" onChange={handleInputChange} placeholder="Email" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Control ref={passwordRef} className="boxInput" name="password" type="password" placeholder="Contraseña" onChange={handleInputChange}/>
-                </Form.Group>
-                
-                <Button className="boton" ref={inputRef} variant="success" type="submit" disabled>
-                    Iniciar sesion
-                </Button>
-                <hr/>
-                <Form.Group>
-                    <p className="urlRecover" onClick={goToRecover}>recuperar contraseña</p>     
-                </Form.Group>
-            </Form>
-        </div>
+        <>
+            <img src={background} alt="background" className="myBackgroundLogin"></img>
+            
+            <div className="divFormLogin">
+                <p className="title"> SGP </p>
+                <Form className="mainForm" onSubmit={goToHome}>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Control ref={emailRef} className="boxInput" name="email" onChange={handleInputChange} placeholder="Email" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Control ref={passwordRef} className="boxInput" name="password" type="password" placeholder="Contraseña" onChange={handleInputChange} />
+                    </Form.Group>
+
+                    <Button className="boton" ref={inputRef} variant="success" type="submit" disabled>
+                        Iniciar sesion
+                    </Button>
+                    <hr />
+                    <Form.Group>
+                        <p className="urlRecover" onClick={goToRecover}>¿Olvidaste tu contraseña?</p>
+                    </Form.Group>
+                </Form>
+            </div>
+        </>
     )
 };
 

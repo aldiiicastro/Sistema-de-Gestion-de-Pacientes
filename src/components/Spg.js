dfc567fe5a8dd93be40a9d331b84512c9268e477
Charly <carlosjoelsalda@gmail.com>
2021-09-25 01:44:58 -0300

    integracion de registro terminada

--- src/components/Spg.js
+++ src/components/Spg.js
@@ -8,6 +8,7 @@ import React from "react";
 import Login from "./Login";
 import PageError from "./PageError";
 import RecoverPassword from "./RecoverPassword";
+import Register from './Register'
 
 const Spg = () => {
   return (
@@ -16,6 +17,7 @@ const Spg = () => {
         <Switch>
           <Route path="/recoverPassword" component={RecoverPassword}></Route>
           <Route path="/error" component={PageError}></Route>
+          <Route path="/register" component={Register}></Route>
           <Route path="/" component={Login}></Route>
         </Switch>
       </Router>

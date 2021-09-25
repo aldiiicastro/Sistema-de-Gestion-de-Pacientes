dbea66aeca85fe41e8ec07f8cf2ab6c3ee148b1f
Aldana Castro <aldanacastro1999@gmail.com>
2021-09-25 18:41:50 -0300

    Principio de Backend

---
+++ sgp/backend/server.js
@@ -0,0 +1,11 @@
+const express =  require('express')
+const app = express()
+require('./database')
+const apiRoutes = require('./routes/apiRoutes');
+
+//Api
+app.use('/api', apiRoutes);
+
+//Connect to PORT
+const PORT = process.env.PORT || 5000
+app.listen(PORT, console.log(`App is running in ${process.env.NODE_ENV} mode on port ${PORT}`))

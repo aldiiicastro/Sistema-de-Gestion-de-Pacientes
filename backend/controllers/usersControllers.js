1d73963255ae4ed2aa5ca6237f2bfc38e54a0313
Charly <carlosjoelsalda@gmail.com>
2021-09-26 04:52:51 -0300

    Agregados status y controllers, endpoints funcionando, tests en postman

--- backend/controllers/usersControllers.js
+++ backend/controllers/usersControllers.js
@@ -2,11 +2,15 @@ const { response } = require('express');
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 
+/**
+ * Como anotación, deberiamos modificar las responses en caso de errores, hoy por hoy esta devolviendo un objeto completo.
+ * Lo mismo para usuarios, los endpoints no deberian devolver el id y contraseñas.
+ */
 
 exports.login_user = async function (req, res) {
     try {
         if (!req.body.email || !req.body.password) {
-            res.status(400).json({
+            return res.status(400).json({
                 response: 'Revisar campos y/o sus datos'
             })
         }
@@ -14,16 +18,20 @@ exports.login_user = async function (req, res) {
 
         findUser ? res.status(200).json({ response: 'usuario logueado' }) : res.status(404).json({ response: 'usuario no encontrado' })
     } catch (error) {
-        res.serverError(error)
+        res.status(500).json({
+            error: error
+        })
     }
 }
-
+// await new Promise(r => setTimeout(r, 6000))
 exports.get_all_users = async function (req, res) {
     try {
         const users = await User.find();
         res.status(200).json({ response: users });
     } catch (error) {
-        res.serverError(error)
+        res.status(500).json({
+            error: error
+        })
     }
 }
 
@@ -31,9 +39,9 @@ exports.get_user_by_id = async function (req, res) {
     try {
         const user = await User.findById(req.params.id);
 
-        user ? res.status(200).json({ response: user }) : res.status(400).json({ response: 'usuario no encontrado' })
+        res.status(200).json({response: 'Usuario encontrado', data: user })
     } catch (error) {
-        res.serverError(error)
+        res.status(404).json(error)
     }
 
 }
@@ -41,7 +49,7 @@ exports.get_user_by_id = async function (req, res) {
 exports.register_user = async function (req, res) {
     try {
         if (!req.body.name || !req.body.email || !req.body.password) {
-            res.status(404).json({
+            return res.status(404).json({
                 response: 'Revisar campos y/o sus datos'
             })
         }
@@ -62,7 +70,9 @@ exports.register_user = async function (req, res) {
 
 
     } catch (error) {
-        res.serverError(error)
+        res.status(500).json({
+            error: error
+        })
     }
 }
 

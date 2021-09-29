682410c84002e6124bb5e058ba1e2505afd6fc04
Charly <carlosjoelsalda@gmail.com>
2021-09-29 02:09:43 -0300

    integracion con lo de ema

--- src/components/Spg.js
+++ src/components/Spg.js
@@ -9,14 +9,17 @@ import Login from "./Login";
 import PageError from "./PageError";
 import RecoverPassword from "./RecoverPassword";
 import Register from './Register'
-import Home from './Home'
+import { Header } from "./Header";
+import Paciente from "./Paciente";
+import { PacientForm } from "./PacientForm";
 
 const Spg = () => {
   return (
     <>
       <Router>
         <Switch>
-          <Route path="/home" component={Home}></Route>
+          <Route path="/ingreso-paciente" component={PacientForm}></Route>
+          <Route path="/lista-pacientes" component={Paciente}></Route>
           <Route path="/recoverPassword" component={RecoverPassword}></Route>
           <Route path="/error" component={PageError}></Route>
           <Route path="/register" component={Register}></Route>

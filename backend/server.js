2ccf0db668f5f28a8d4f7dbf236edef57abf0373
Aldana Castro <aldanacastro1999@gmail.com>
2021-10-08 00:34:19 -0300

    Deploying into heroku change server.js

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,17 @@ app.use(express.json())
 app.use(cors())
 app.use('/api', apiRoutes);
 
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+  
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+
 //Connect to PORT
 //const PORT = process.env.PORT || 5000
 // app.listen(PORT, console.log(`App is running in ${process.env.NODE_ENV} mode on port ${PORT}`))

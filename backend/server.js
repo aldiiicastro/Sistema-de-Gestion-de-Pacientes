eba41c83bc33b87cacff9592390ddb16364ada8a
Aldana Castro <aldanacastro1999@gmail.com>
2021-10-08 20:51:49 -0300

    Deployin into heroku

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,20 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const path = require('path');
-const app = express();
-var env = process.env.NODE_ENV
-require('./database');
+const express =  require('express')
+const app = express()
+require('./database')
 const apiRoutes = require('./routes/apiRoutes');
-app.use(cors());
-//Cargar middlewares
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-//Activar el cors
-// Configurar cabeceras y cors
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
-
-if (env === 'production') {
-    app.use(express.static(path.join(__dirname, '../build')))
-}
+const timeout = require('connect-timeout');
+const cors = require('cors')
 
 //Api
+app.use(timeout('5s'))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(cors())
 app.use('/api', apiRoutes);
 
-
+//Connect to PORT
+//const PORT = process.env.PORT || 5000
+// app.listen(PORT, console.log(`App is running in ${process.env.NODE_ENV} mode on port ${PORT}`))
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);

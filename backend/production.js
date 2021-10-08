1c370b76972d994739932d1ddf533c801a249924
Aldana Castro <aldanacastro1999@gmail.com>
2021-10-08 00:58:40 -0300

    Deploying into heroku change server.js

---
+++ backend/production.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI:
+      'mongodb+srv://sgp:interfaces@cluster0.javrm.mongodb.net/sgp?retryWrites=true&w=majority'
+  };
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,18 @@ require('./database')
 const apiRoutes = require('./routes/apiRoutes');
 const timeout = require('connect-timeout');
 const cors = require('cors')
+const db = require('./production').mongoURI;
+
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  // use a promise to check if success
+  .then(() => console.log('MongoDB Connected!'))
+  .catch(error => console.log('MongoDB did not connect: ', error));
+
 
 //Api
 app.use(timeout('5s'))
@@ -12,10 +24,15 @@ app.use(express.json())
 app.use(cors())
 app.use('/api', apiRoutes);
 
-if (env === 'production') {
-    app.use(express.static(path.join(__dirname, '../build')))
-}
-
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (request, response) => {
+      response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 //Connect to PORT
 //const PORT = process.env.PORT || 5000
 // app.listen(PORT, console.log(`App is running in ${process.env.NODE_ENV} mode on port ${PORT}`))

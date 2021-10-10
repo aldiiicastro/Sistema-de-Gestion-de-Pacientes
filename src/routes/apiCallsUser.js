93cc56dae2cb15c34fef3c0b88b33b7cdf49d52b
Aldana Castro <aldanacastro1999@gmail.com>
2021-10-10 19:50:16 -0300

    Deploying into heroku change

--- src/routes/apiCallsUser.js
+++ src/routes/apiCallsUser.js
@@ -1,19 +1,19 @@
 import axios from "axios";
 
-const basicUrl = 'http://sistema-gestion-paciente.herokuapp.com/api';
+// const basicUrl = 'http://sistema-gestion-paciente.herokuapp.com/api';
 
 export const login = (obj) => {
-    return axios.post(basicUrl + `/login`, obj)
+    return axios.post(`/login`, obj)
 }
 
 export const register = (obj) => {
-    return axios.post(basicUrl + `/register`, obj)
+    return axios.post(`/register`, obj)
 }
 
 export const mailRegistered = (obj) => {
-    return axios.post(basicUrl + `/mailRegistered`, obj)
+    return axios.post(`/mailRegistered`, obj)
 }
 
 export const changePassword =  (obj) => {
-    return axios.put(basicUrl + `/changePassword`, obj)
+    return axios.put(`/changePassword`, obj)
 }

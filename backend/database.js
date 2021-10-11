a37c9b912c05ae659504ceddd7088c6794c2a3f7
Aldana Castro <aldanacastro1999@gmail.com>
2021-10-11 16:21:15 -0300

    Deploying into heroku change

--- backend/database.js
+++ backend/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 const connection = "mongodb+srv://sgp:interfaces@cluster0.javrm.mongodb.net/sgp?retryWrites=true&w=majority";
-mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+mongoose.connect(connection)
     .then(() => console.log("Database Connected Successfully"))
     .catch(err => console.log(err));

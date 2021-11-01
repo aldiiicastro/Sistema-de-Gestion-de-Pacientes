c008045f032eb323959c1eb6458159a02e6c52b0
EmanuelRighi <righiemanue@gmail.com>
2021-10-31 23:53:27 -0300

    SGP-160 historia de usuario resuelta con tests

--- sgp/cypress/integration/test-front/WattingList.spec.js
+++ sgp/cypress/integration/test-front/WattingList.spec.js
@@ -8,8 +8,7 @@ describe('WattingPatient', () =>
         cy.visit('http://localhost:5000/add-patient');
         cy.get('[id= ControlTextAreaNN1]').type("Emanuel");
         cy.get('[id= ControlTextAreaNN2]').type("Righi");
-        cy.get('[id= ControlTextAreaNN3]').type("45869547");
-        cy.scrollTo(0, 500)
+        cy.get('[id= ControlTextAreaNN3]').type("11111111");
         cy.get('[id= ingresarButton]').click();
     })
 
@@ -18,7 +17,14 @@ describe('WattingPatient', () =>
         cy.visit('http://localhost:5000/watting-list');
         cy.contains("Emanuel");
         cy.contains("Righi");
-        cy.contains("45869547");
+        cy.contains("11111111");
     })
+
+   
+
+
+   
+
+
 
 })

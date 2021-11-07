ae00835505946745529fb7a40ac37a2e77355013
Charly <carlosjoelsalda@gmail.com>
2021-11-07 02:51:48 -0300

    SGP-37 primeros avances

--- sgp/src/components/Graphs/Stadistics.js
+++ sgp/src/components/Graphs/Stadistics.js
@@ -1,65 +1,91 @@
-import React from "react";
-import '../../styles/PatientList.css';
-import NavegationRecepcionista from '../Navegation/RecepcionistNavegation';
-import  {Pie} from 'react-chartjs-2'
-import  {Bar} from 'react-chartjs-2'
+import React, { useEffect, useState } from "react";
+import "../../styles/PatientList.css";
+import { pacientesEsperando } from "../../routes/apiCallsPatient";
+import NavegationRecepcionista from "../Navegation/RecepcionistNavegation";
+import { useHistory } from "react-router";
+import { atenderPaciente } from "../../routes/apiCallsPatient";
+import {
+  faClinicMedical,
+  faUserCheck,
+} from "@fortawesome/free-solid-svg-icons";
+import { faUserPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Pie } from "react-chartjs-2";
+//import  {Bar} from 'react-chartjs-2'
+const Stadistics = () => {
+  const [data, setData] = useState([]);
+  const data2 = {
+    type: "pie",
+    labels: ["Pacientes en espera"],
+    datasets: [
+      {
+        data: [data.length], //modificar el datasets para traer la informacion real
+        backgroundColor: [ "rgba(255, 206, 86, 0.2)"],
+        borderColor: ["rgba(255, 206, 86, 1)"],
+        borderWidth: 1,
+      },
+    ],
+  };
+  /*     const data3 ={
+        type: 'bar',
+        labels:['Pacientes atendidos','Pacientes en espera'],
+        datasets:[{data3:[80,20],
+        backgroundColor:['rgba(255, 99, 132, 1)',
+        'rgba(255, 206, 86, 0.2)',
+      ],borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(255, 206, 86, 1)'],
+      borderWidth: 1
+        }]
+      }; */
+  const opciones = {
+    responsive: false,
+  };
+  /*   const opciones2={
+      responsive: false
+    } */
+  useEffect(() => {
+    pacientesEsperando().then((res) => {
+      setData(res.data.data);
+    });
+  }, []);
 
-const Stadistics = () => { 
-  
-        const data ={ 
-          type: 'pie',
-          labels:['Pacientes atendidos','Pacientes en espera'],
-          datasets:[{data:[80,20], //modificar el datasets para traer la informacion real 
-          backgroundColor:['rgba(255, 99, 132, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-        ],borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(255, 206, 86, 1)'],
-        borderWidth: 1
-          }]
-        };
+  return (
+    <>
+      {data.length !== 0 ? (
+        <div>
+          <div className="centro">
+            <h3>Pacientes en Lista de Espera</h3>
+            <br />
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Nombre</th>
+                  <th>Apellido</th>
+                  <th>DNI</th>
+                  <th>Accion</th>
+                </tr>
+              </thead>
+            </table>
+          </div>
+        </div>
+      ) : (
+          <div className="noPatient">
+            <FontAwesomeIcon icon={faClinicMedical} size="4x" />
+            <p>No hay pacientes</p>
+          </div>
+      )}
 
-        const data2 ={
-          type: 'bar',
-          labels:['Pacientes atendidos','Pacientes en espera'],
-          datasets:[{data:[80,20],
-          backgroundColor:['rgba(255, 99, 132, 1)',
-          'rgba(255, 206, 86, 0.2)',
-        ],borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(255, 206, 86, 1)'],
-        borderWidth: 1
-          }]
-        };
-        const opciones={
-          responsive: false
-        }
-        const opciones2={
-          responsive: false
-        }
-        
-            return (
-                <React.Fragment>
+      <h3>Graficos de Pacientes en lista de espera y atendidos</h3>
+      <div className="App">
+        <h3>Graficos de Pacientes en lista de espera y atendidos</h3>
+      </div>
 
-                <NavegationRecepcionista/>
-                <h3>Graficos de Pacientes en lista de espera y atendidos</h3>
-                <div className="App">
-                <h3>Graficos de Pacientes en lista de espera y atendidos</h3>
-               </div>
-               
-               <div className="App">
-               
-                 <Pie data={data} opciones={opciones}/>
-                 <Bar data={data2} opciones={opciones2}/>
-
-                </div>
-                </React.Fragment>
-               
-             
-
-             );
-
-    
-}
-
+      <div className="App">
+        <Pie data={data2} opciones={opciones} />
+        {/* <Bar data={data2} opciones={opciones2}/> */}
+      </div>
+    </>
+  );
+};
 export default Stadistics;
